1aedaa121f735157b674c18b840956217d3e16a6
Daniil Skorodumov <skorodumovdg@gmail.com>
2024-09-22 16:02:25 +0300

    Updated admin panel

--- task_manager/main/admin.py
+++ task_manager/main/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+from .models import Tag
+from .models import Task
 
-admin.site.register(User, UserAdmin)
+
+class TaskManagerAdminSite(admin.AdminSite):
+    pass
+
+
+task_manager_admin_site = TaskManagerAdminSite(name="Task manager admin")
+
+
+@admin.register(User, site=task_manager_admin_site)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "role", "is_staff")
+
+
+@admin.register(Tag, site=task_manager_admin_site)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+
+
+@admin.register(Task, site=task_manager_admin_site)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "assignee",
+        "state",
+        "priority",
+        "created_at",
+        "updated_at",
+        "expired_at",
+    )
