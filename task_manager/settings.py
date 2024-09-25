4e50f49afb7e2ee73547566eb4c83a6f336508dc
Daniil Skorodumov <skorodumovdg@gmail.com>
2024-09-25 22:02:09 +0300

    Configured rest_framework & django_filters

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -41,7 +41,13 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "task_manager.main",
+    "rest_framework",
+    "django_filters",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
