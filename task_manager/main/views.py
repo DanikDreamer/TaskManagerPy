c09fd219eb5e9640cfef37cc98e475c3a3dc1d77
Daniil Skorodumov <skorodumovdg@gmail.com>
2024-09-26 21:15:30 +0300

    Added FilterSets

--- task_manager/main/views.py
+++ task_manager/main/views.py
@@ -1,11 +1,32 @@
+import django_filters
 from rest_framework import viewsets
 from .models import User, Tag, Task
 from .serializers import UserSerializer, TagSerializer, TaskSerializer
 
 
+class UserFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr="icontains")
+
+    class Meta:
+        model = User
+        fields = ("name",)
+
+
+class TaskFilter(django_filters.FilterSet):
+    state = django_filters.ChoiceFilter(choices=Task.States.choices)
+    tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all())
+    author = django_filters.ChoiceFilter(queryset=User.objects.all())
+    assignee = django_filters.ChoiceFilter(queryset=User.objects.all())
+
+    class Meta:
+        model = Task
+        fields = ("state", "tags", "author", "assignee")
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.order_by("id")
     serializer_class = UserSerializer
+    filterset_class = UserFilter
 
 
 class TagViewSet(viewsets.ModelViewSet):
@@ -20,3 +41,4 @@ class TaskViewSet(viewsets.ModelViewSet):
         .order_by("id")
     )
     serializer_class = TaskSerializer
+    filterset_class = TaskFilter
