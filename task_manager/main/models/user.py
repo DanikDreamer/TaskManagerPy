b9ca3524e92ab2ca92e8c23937ed8b4ed21319f3
Daniil Skorodumov <skorodumovdg@gmail.com>
2024-10-01 19:56:44 +0300

    Altered field phone on user

--- task_manager/main/models/user.py
+++ task_manager/main/models/user.py
@@ -12,4 +12,4 @@ class User(AbstractUser):
         max_length=255, default=Roles.DEVELOPER, choices=Roles.choices
     )
     date_of_birth = models.DateField(null=True, blank=True)
-    phone = models.CharField(max_length=15, null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
