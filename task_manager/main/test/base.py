6e0b4cd9eee9698ca4fe72a92813578db3faedac
Daniil Skorodumov <skorodumovdg@gmail.com>
2024-10-13 21:51:48 +0300

    Added mail service test

--- task_manager/main/test/base.py
+++ task_manager/main/test/base.py
@@ -3,13 +3,65 @@ from typing import List, Union
 
 from django.urls import reverse
 from rest_framework.test import APIClient, APITestCase
+from rest_framework.response import Response
 
 from task_manager.main.models import User
+from task_manager.main.test.factories import UserFactory, TagFactory, TaskFactory
+
+
+class ActionClient:
+    def __init__(self, api_client: APIClient, user: User) -> None:
+        self.api_client = api_client
+        self.user = user
+
+    def authenticate_user(self) -> None:
+        if self.user:
+            self.api_client.force_authenticate(self.user)
+
+    def request_create_user(self, **attributes) -> Response:
+        url = reverse("users-list")
+        return self.api_client.post(url, data=attributes)
+
+    def create_user(self) -> dict:
+        user_attributes = UserFactory.build()
+        user_data = {
+            "username": user_attributes.username,
+            "first_name": user_attributes.first_name,
+            "last_name": user_attributes.last_name,
+            "email": user_attributes.email,
+            "date_of_birth": user_attributes.date_of_birth,
+            "phone": user_attributes.phone,
+        }
+        response = self.request_create_user(**user_data)
+        assert response.status_code == HTTPStatus.CREATED, response.content
+        return response.data
+
+    def request_create_task(self, **attributes) -> Response:
+        url = reverse("tasks-list")
+        return self.api_client.post(url, data=attributes)
+
+    def create_task(self, **attributes) -> dict:
+        user = UserFactory()
+        tag = TagFactory()
+        task_attributes = TaskFactory.build()
+        task_data = {
+            "title": task_attributes.title,
+            "description": task_attributes.description,
+            "state": task_attributes.state,
+            "priority": task_attributes.priority,
+            "author": user.id,
+            "tags": [tag.id],
+        }
+        task_data.update(attributes)
+        response = self.request_create_task(**task_data)
+        assert response.status_code == HTTPStatus.CREATED, response.content
+        return response.data
 
 
 class TestViewSetBase(APITestCase):
     user: User = None
     client: APIClient = None
+    action_client: ActionClient = None
     basename: str
 
     @classmethod
@@ -17,6 +69,8 @@ class TestViewSetBase(APITestCase):
         super().setUpTestData()
         cls.user = cls.create_api_user()
         cls.client = APIClient()
+        cls.action_client = ActionClient(cls.client, cls.user)
+        cls.action_client.authenticate_user()
 
     @classmethod
     def create_api_user(cls) -> User:
