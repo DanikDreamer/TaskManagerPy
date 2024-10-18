0c53eddadf3ae1583d6b53ca4c40cd838f3fa8c6
Daniil Skorodumov <skorodumovdg@gmail.com>
2024-10-18 10:37:27 +0300

    Added mixins to SingleResource

--- task_manager/main/services/single_resource.py
+++ task_manager/main/services/single_resource.py
@@ -1,7 +1,39 @@
 import copy
-from typing import Any, List
+from typing import Any, List, TYPE_CHECKING
 
-from rest_framework import routers
+from rest_framework import routers, viewsets
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+if TYPE_CHECKING:
+    BaseViewMixinBaseClass = viewsets.GenericViewSet
+else:
+    BaseViewMixinBaseClass = object
+
+
+class SingleResourceMixin(BaseViewMixinBaseClass):
+    pagination_class = None
+
+    def list(self, _: Request, *__: Any, **___: Any) -> Response:
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+class SingleResourceUpdateMixin(BaseViewMixinBaseClass):
+    def bulk_update(self, request: Request, *_: Any, **kwargs: Any) -> Response:
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def partial_bulk_update(
+        self, request: Request, *args: Any, **kwargs: Any
+    ) -> Response:
+        kwargs["partial"] = True
+        return self.bulk_update(request, *args, **kwargs)
 
 
 class BulkRouter(routers.SimpleRouter):
